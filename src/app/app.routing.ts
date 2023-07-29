9e37090204299a40a04892e5e67cb6e9863f1877
Matheus Soares <m.soares.ms@outlook.com>
2023-07-29 15:06:04 -0400

    initial commit

---
+++ src/app/app.routing.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule, } from '@angular/common';
+import { BrowserModule } from '@angular/platform-browser';
+import { Routes, RouterModule } from '@angular/router';
+import { LandingComponent } from './examples/landing/landing.component';
+
+const routes: Routes = [
+    { path: '', component: LandingComponent }
+];
+
+@NgModule({
+    imports: [
+        CommonModule,
+        BrowserModule,
+        RouterModule.forRoot(routes)
+    ],
+    exports: [
+    ],
+})
+export class AppRoutingModule { }
