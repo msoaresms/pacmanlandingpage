dbd08b0bd42d1d6ba933e8eeb5d4e8fb78f5b37e
Matheus Soares <m.soares.ms@outlook.com>
2026-04-18 23:38:01 -0400

    angular 16 update

--- src/app/examples/examples.module.ts
+++ src/app/examples/examples.module.ts
@@ -1,9 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { NouisliderModule } from 'ng2-nouislider';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { JwBootstrapSwitchNg2Module } from 'jw-bootstrap-switch-ng2';
 
 import { LandingComponent } from './landing/landing.component';
 
@@ -11,9 +9,7 @@ import { LandingComponent } from './landing/landing.component';
     imports: [
         CommonModule,
         FormsModule,
-        NgbModule,
-        NouisliderModule,
-        JwBootstrapSwitchNg2Module,
+        NgbModule
     ],
     declarations: [
         LandingComponent
